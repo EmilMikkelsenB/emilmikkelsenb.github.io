7c9d39fe88726ff0aea8635e832cde2e098b93b0
awiant <emil.mikkelsenbage15@gmail.com>
2023-09-26 11:02:23 +0200

    complet render
    position it better

--- ball.js
+++ ball.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -8,20 +7,55 @@ const renderer = new THREE.WebGLRenderer({ canvas: bg, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Create a sphere geometry
+const geometry = new THREE.SphereGeometry(1, 128, 128);
 
-camera.position.z = 50;
+// Create a custom shader material
+const shaderMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { value: 0.0 }, // Time uniform for animation
+    },
+    vertexShader: `
+        uniform float time;
+        void main() {
+            vec3 newPosition = position;
+            // Add a sine wave effect to the Y coordinate of the points with animation
+            float waveFrequency = 5.0;
+            float waveAmplitude = 0.1;
+            newPosition.y += sin(newPosition.x * waveFrequency + time) * waveAmplitude;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 0.5);
+            gl_PointSize = 2.0; // Adjust point size as needed
+        }
+    `,
+    fragmentShader: `
+        void main() {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);}
+    `,
+});
 
-function animate() {
+// Create a points object with the shader material
+const sphere = new THREE.Points(geometry, shaderMaterial);
+
+// Add the sphere to the scene
+scene.add(sphere);
+
+// Set the camera position
+camera.position.z = 5;
+
+// Create an animation loop
+const animate = () => {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // Update the time uniform for the shader
+    shaderMaterial.uniforms.time.value += 0.01; // You can adjust the speed of the animation
 
+    // Rotate the sphere
+    sphere.rotation.x += 0.005;
+    sphere.rotation.y += 0.005;
+
+    // Render the scene
     renderer.render(scene, camera);
-}
+};
 
+// Call the animate function to start the animation loop
 animate();
