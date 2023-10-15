f7ec58dbfe3e0e6f3c0751ab9f6e44247c63740b
awiant <emil.mikkelsenbage15@gmail.com>
2023-10-15 23:08:23 +0200

    progress on landing page
    fix mobile ver nav bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'C:/emilmikkelsenbage/portfolio/index.html'; /* The text */
+var txt = 'C:/users/emilmikkelsenbage/portfolio/index.html'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
