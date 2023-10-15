1e4b3f060425a5de3c9ee8f1b3de4e27b9694490
awiant <emil.mikkelsenbage15@gmail.com>
2023-10-15 21:30:00 +0200

    Groundwork for new page

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+var i = 0;
+var txt = 'C:/emilmikkelsenbage/portfolio/index.html'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("navBrand").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+};
+
+window.onload = function () {
+    typeWriter();
+};
+
+
+function showObject(element) {
+    let object = document.getElementById(element);
+    let elementsToHide = document.querySelectorAll('.toggleable');
+    elementsToHide.forEach(function (el) {
+        if (el !== object) {
+            el.classList.add('d-none');
+        }
+    });
+
+    if (object.classList.contains("d-none")) {
+        elementsToHide.forEach(function (el) {
+            el.classList.add('d-none');
+        });
+        object.classList.remove('d-none');
+    } else {
+        object.classList.add("d-none");
+    }
+};
