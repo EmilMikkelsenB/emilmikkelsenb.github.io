7b56fafa3e14c758394235d3a55ef4d96c59a82c
awiant <emil.mikkelsenbage15@gmail.com>
2023-10-18 01:51:59 +0200

    adjusted images, added spacers and added copyright

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 function changeText(containerNum) {
+
     let textId = document.getElementById(containerNum);
     let textFromId = textId.textContent;
-    console.log(textFromId)
-    document.getElementById("head").innerHTML = textFromId;
+    let headElement = document.getElementById("head");
+    headElement.classList.add('change-color');
+
+
+    setTimeout(function () {
+        headElement.innerHTML = textFromId;
+        headElement.classList.remove('change-color');
+    }, 200);
 };
